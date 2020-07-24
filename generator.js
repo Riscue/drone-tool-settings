d22c72934c0ae3746c035543a0567591ff75f899
İbrahim Akyel <ibrahim.akyel@btk.gov.tr>
2020-07-24 17:30:35 +0300

    Initial commit

---
+++ generator.js
@@ -0,0 +1,86 @@
+'use strict';
+const Sqrl = require('squirrelly');
+const path = require('path');
+const fs = require('fs');
+
+Sqrl.defaultConfig.autoEscape = false;
+
+function generateMavenSettings(settings) {
+    let outputPath = settings.filePath || path.join('.m2', 'settings.xml');
+
+    if (process.env['UNIT_TEST'] !== 'true') {
+        console.log(`Maven: Creating ${outputPath} file...`);
+    }
+
+    let settingsTemplate = fs.readFileSync(path.join(__dirname, 'templates', 'maven', 'settings.xml'), 'utf8');
+    let settingsFile = Sqrl.render(settingsTemplate, settings);
+
+    ensureDirectoryExistence(outputPath);
+    fs.writeFileSync(outputPath, settingsFile);
+
+    if (process.env['UNIT_TEST'] !== 'true') {
+        console.log(`Maven: The ${outputPath} file has been created.`);
+    }
+}
+
+function generateNpmSettings(settings) {
+    let outputPath = settings.filePath || path.join('.npmrc');
+
+    if (process.env['UNIT_TEST'] !== 'true') {
+        console.log(`Npm: Creating ${outputPath} file...`);
+    }
+
+    settings.registry = fixUrl(settings.registry);
+    settings.proxy = fixUrl(settings.proxy);
+    settings.http_proxy = fixUrl(settings.http_proxy);
+    settings.https_proxy = fixUrl(settings.https_proxy);
+
+    let settingsTemplate = fs.readFileSync(path.join(__dirname, 'templates', 'npm', '.npmrc'), 'utf8');
+    let settingsFile = Sqrl.render(settingsTemplate, settings);
+
+    ensureDirectoryExistence(outputPath);
+    fs.writeFileSync(outputPath, settingsFile);
+
+    if (process.env['UNIT_TEST'] !== 'true') {
+        console.log(`Npm: The ${outputPath} file has been created.`);
+    }
+}
+
+function generateSettings() {
+    let settings = require('drone-env-parser').parseEnvs();
+
+    if (Object.prototype.hasOwnProperty.call(settings, 'maven')) {
+        generateMavenSettings(settings['maven']);
+    }
+
+    if (Object.prototype.hasOwnProperty.call(settings, 'npm')) {
+        generateNpmSettings(settings['npm']);
+    }
+}
+
+function fixUrl(urlObj) {
+    if (!urlObj) {
+        return;
+    }
+    const parsedUrl = require('url').parse(urlObj.url, true);
+
+    let userpass;
+    if (urlObj.username || urlObj.password) {
+        userpass = `${urlObj.username}${urlObj.password ? `:${urlObj.password}` : ''}@`;
+    }
+
+    return `${parsedUrl.protocol || 'http:'}//${userpass || ''}${parsedUrl.host || ''}${parsedUrl.pathname || ''}${parsedUrl.search || ''}`;
+}
+
+function ensureDirectoryExistence(filePath) {
+    const dirname = path.dirname(filePath);
+    if (fs.existsSync(dirname)) {
+        return;
+    }
+    ensureDirectoryExistence(dirname);
+    fs.mkdirSync(dirname);
+}
+
+module.exports = {
+    generateSettings
+};
